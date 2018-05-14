2637def386ad9ec2f615638c481dc4bffeda756f
marsxck <q271537342@163.com>
2018-05-14 17:54:57 +0800

    tcpserver

---
+++ server/tcp-server.py
@@ -0,0 +1,21 @@
+import sys
+import time
+import gevent
+from gevent import socket,monkey
+monkey.patch_all()
+def handle_request(conn):
+    while True:
+        data=conn.recv(1024)
+        if not data:
+            conn.close()
+            break
+        print("recv:",data)
+def server(port):
+    s=socket.socket()
+    s.bind(('172.21.16.2',port))
+    s.listen(5)
+    while True:
+        cli,addr=s.accept()
+        gevent.spawn(handle_request,cli)
+if __name__=='__main__':
+    server(7788)
